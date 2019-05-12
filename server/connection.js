530f6d2f9a7856272df397be3ca6acd1b3f72282
ninja <ebecker03@msn.com>
2019-05-12 13:42:55 -0500

    changed connections

--- server/connection.js
+++ server/connection.js
@@ -2,10 +2,13 @@ const mysql = require("mysql");
 let pool = mysql.createPool({
     connectionLimit: 10,
     host: "process.env.DBHOST",
-    user: "process.env.DBUSER",
-    password: "process.env.DBPASSWORD",
     database: "process.env.DBNAME",
+    password: "process.env.DBPASSWORD",
     port: "process.env.DBPORT",
+    user: "process.env.DBUSER",
+    
+    
+    
     
 
 })
