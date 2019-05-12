93a87bff5ef3630094d24592ec009bbd156f50e2
ninja <ebecker03@msn.com>
2019-05-12 13:48:02 -0500

    connections

--- server/connection.js
+++ server/connection.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql");
 let pool = mysql.createPool({
     connectionLimit: 10,
-    host: "process.env.DBHOST",
-    database: "process.env.DBNAME",
-    password: "process.env.DBPASSWORD",
-    port: "process.env.DBPORT",
-    user: "process.env.DBUSER",
+    host: "process.env.sql9.freemysqlhosting.net",
+    database: "process.env.sql9291340",
+    password: "process.env.tpxD2T4wuz",
+    port: "process.env.3306",
+    user: "process.env.sql9291340",
     
     
     
